e91e5095e167f9a50d238166c1fbddfa444b6060
Iyamuremye Benjamin <yamubbenjamin@gmail.com>
2022-04-05 13:43:28 +0200

    fixing duplication emails

--- routes/application.js
+++ routes/application.js
@@ -3,11 +3,22 @@ const Application = require("../models/ApplicationForm");
 
 router.post("/apply", async (req, res) => {
   try {
-    console.log(req.body);
-    const savedApplication = await Application.create(req.body);
-    return res
-      .status(200)
-      .json({ message: "success application created", data: savedApplication });
+    // console.log(req.body);
+    const isApplicantExist = await Application.findOne({
+      email: req.body.email.trim(),
+    });
+    if (isApplicantExist) {
+      return res.status(202).json({
+        message: `Hey! ${isApplicantExist?.fistName}your application has been arleady received succesfully,  Now schedule for interview`,
+      });
+    } else {
+      const savedApplication = await Application.create(req.body);
+      return res.status(200).json({
+        message: `Thank you! ${isApplicantExist?.fistName}, your application has been received successfuly, Kindly make schedule for interview`,
+
+        data: savedApplication,
+      });
+    }
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -15,9 +26,11 @@ router.post("/apply", async (req, res) => {
 router.get("/all", async (req, res) => {
   try {
     const apps = await Application.find();
-    return res
-      .status(200)
-      .json({ message: "success retreived all application",results:apps.length, data: apps });
+    return res.status(200).json({
+      message: "success retreived all application",
+      results: apps.length,
+      data: apps,
+    });
   } catch (err) {
     return res.status(500).json(err);
   }
